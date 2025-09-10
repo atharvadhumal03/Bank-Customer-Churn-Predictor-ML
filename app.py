871cac89191715c562a8556c7fa0c21c2a8afdf5
Atharva Dhumal <atharvapdhumal@gmail.com>
2025-09-10 11:23:14 -0700

    deployment - added CSS styling.

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ st.markdown("<h1 style='text-align: center;'>Bank Customer Churn Prediction</h1>
 st.markdown(
     """
     
-    <div style='text-align: center; color: gray; font-size: 13px;'>
+    <div style='text-align: center; color: black; font-size: 13px;'>
         © Atharva Dhumal | A bank customer churn predictor model.
     </div>
     <hr style="margin-top: 20px;"/>
@@ -28,11 +28,12 @@ st.markdown(
 """
     <style>
     .stApp {
-        background-color: #262e36;
+        background-color: #005EB8;
     }
     </style>
     """,
     unsafe_allow_html=True
+    # 31487A
 )
 
 st.markdown(
@@ -40,18 +41,27 @@ st.markdown(
     <style>
     .stButton > button {
         width: 100%;
-        background-color: #FF4B4B;
-        color: white;
-        height: 3em;
+        background-color: white;
+        color: black;
+        height: 2.5em;
         border-radius: 10px;
         border: none;
         font-size: 20px;
         font-weight: bold;
         transition: all 0.3s;
-        margin-top: 20px;
+        margin-top: 25px;
     } 
+
+     .stButton > button:hover {
+        background-color: #FF6B6B;
+        color: white;
+        transform: translateY(-2px);
+        box-shadow: 0 6px 12px rgba(255, 75, 75, 0.4);
+    }
+
     </style>
-    """
+    """,
+    unsafe_allow_html=True
 )
 
 # --- Inputs ---
@@ -96,4 +106,9 @@ if st.button("Predict Churn"):
     input_ready_data = model_input(credit_score, geography, gender, age, tenure, balance, num_products, has_card, is_active, estimated_salary)
     prediction = predict_churn(input_ready_data, model_pipeline)
     
-    st.success(f"Prediction: {prediction}")
+    if prediction[0] == 0:
+        st.success(f"Prediction: Given customer will not churn!")
+    else:
+        st.info(f"Prediction: Given customer will churn!")
+
+    
